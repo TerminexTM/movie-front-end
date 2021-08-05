c480bc19ea0ee88116d9ca1440e3927552692f1d
dooster <dustin2burns@yahoo.com>
2021-08-05 14:47:48 -0500

    have useStates mostly setup

--- front-end-movie/src/App.js
+++ front-end-movie/src/App.js
@@ -5,6 +5,13 @@ import axios from 'axios'
 
 
 const App = () => {
+   let [newTitle, setNewTitle] = useState('');
+   let [newImage, setNewImage] = useState('');
+   let [newReleaseDate, setNewReleaseDate] = useState('');
+   let [newDescription, setNewDescription] = useState('');
+   let [newCategory, setNewCategory] = useState('');
+   let [newRating, setNewRating] = useState('');
+   let [newReview, setNewReview] = useState('');
    return (
       <h1>Movie App is Online</h1>
    )
