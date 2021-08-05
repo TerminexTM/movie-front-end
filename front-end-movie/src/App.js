6fc834c48ce2854f62879f5dcbf850bbdbccc038
dooster <dustin2burns@yahoo.com>
2021-08-05 14:36:07 -0500

    setting up dev with the same basline code

---
+++ front-end-movie/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
+
+
+const App = () => {
+   return (
+      <h1>Movie App is Online</h1>
+   )
+}
+export default App;
