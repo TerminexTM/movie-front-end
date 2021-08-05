6372b37894fd859fee607fd17ef9a1212cd9ab39
dooster <dustin2burns@yahoo.com>
2021-08-05 15:15:51 -0500

    testing a submit

--- front-end-movie/src/App.js
+++ front-end-movie/src/App.js
@@ -1,19 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react'
-import axios from 'axios'
 
-
-const App = () => {
-   let [newTitle, setNewTitle] = useState('');
-   let [newImage, setNewImage] = useState('');
-   let [newReleaseDate, setNewReleaseDate] = useState('');
-   let [newDescription, setNewDescription] = useState('');
-   let [newCategory, setNewCategory] = useState('');
-   let [newRating, setNewRating] = useState('');
-   let [newReview, setNewReview] = useState('');
-   return (
-      <h1>Movie App is Online</h1>
-   )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
