93a325f80cd3fab6b8e0b08875fa4a858136dfe1
kristianreyes7 <kristianreyes7@icloud.com>
2021-08-05 13:36:49 -0700

    create form started

--- front-end-movie/src/App.js
+++ front-end-movie/src/App.js
@@ -17,12 +17,37 @@ const App = () => {
    let [movies, setMovies] = useState([]);
 
 
-   const handleMovies = (event)
+
 
 
    //JSX RETURN
    return (
+     <>
       <h1>Movie App is Online</h1>
+      <section className="newMovieForm">
+        <h2>Create A New Movie Review</h2>
+        <form>
+          Title: <input type="text"/><br/>
+          Image: <input type="url"/><br/>
+          Release Date: <input type="date"/><br/>
+          <label for="description">Description: </label><br/>
+          <textarea id="description" rows="5" cols="33"></textarea><br/>
+          <label for="category">Category: </label>
+          <select id="category">
+            <option>Action</option>
+            <option>Comedy</option>
+            <option>Documentary</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+            <option>Romance</option>
+          </select><br/>
+          Rating: <input type="number"/><br/>
+          <label for="review">Review: </label><br/>
+          <textarea id="review" rows="5" cols="33"></textarea><br/>
+        </form>
+      </section>
+      </>
    )
 }
 
